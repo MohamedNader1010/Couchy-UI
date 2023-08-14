20fd51c6f729688b8ceffb5236309e9f8897d4cf
Mohamed Nader <m7mdnader1010@gmail.com>
2023-08-15 02:27:49 +0300

    initial commit

---
+++ src/shared/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  items!: MenuItem[];
+
+  @ViewChild('menubutton') menuButton!: ElementRef;
+
+  @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+
+  @ViewChild('topbarmenu') menu!: ElementRef;
+
+constructor(public el: ElementRef) {}
+}
