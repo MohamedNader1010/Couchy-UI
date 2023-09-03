8b33e2336853ac377f5a96b672e9f4402e627801
Mohamed Nader <m7mdnader1010@gmail.com>
2023-09-03 12:52:10 +0300

    fix error

--- src/core/services/auth.service.ts
+++ src/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { GenericService } from './generic.service';
 import { ResponseCode } from 'src/modules/shared/enums/response.enum';
-import * as CryptoJS from 'crypto-js';
+
 import { environment } from 'src/environment';
 @Injectable({
   providedIn: 'root',
@@ -33,22 +33,6 @@ export class AuthService {
   public clearSession() {
     sessionStorage.clear();
   }
-  public setClaims(id: number) {
-    this._adminService.setControllerName('User/GetAdminPermission');
-    this._adminService.getById(id).subscribe((result) => {
-      if (result.code == ResponseCode.Success) {
-        const encryptedData = CryptoJS.AES.encrypt(result.body, this._cryptoSecretKey).toString();
-        sessionStorage.setItem('claims', encryptedData);
-      } else {
-        sessionStorage.setItem('claims', '');
-      }
-    });
-  }
-  public getClaims(): string {
-    const encryptedClaims = sessionStorage.getItem('claims');
-    if (encryptedClaims) {
-      const bytes = CryptoJS.AES.decrypt(encryptedClaims, this._cryptoSecretKey);
-      return bytes.toString(CryptoJS.enc.Utf8);
-    } else return '';
-  }
+ 
+
 }
